d4f22d51ce67f43486191098b2a9233a8756265a
Saakshaat <saakshaat2001@gmail.com>
2020-12-05 18:20:44 -0800

    Update index page

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,25 @@ export async function getStaticProps() {
   };
 }
 
+function randomFact() {
+  const facts = [
+    "humans are the only animals that blush",
+    'the "M\'s" in M&Ms stand for "Mars" and "Murrie."',
+    'the dot over the lower case "i" or "j" is known as a "tittle."',
+    "python was Guido Van Rossum's hobby project",
+    'typing "import this" renders Tim Peter\'s "The Zen of Python"',
+    'a group of penguins in the water is called a "raft" but on land they\'re called a "waddle"',
+    "a quine is a computer program capable of printing it's own source",
+    "Next.js automatically creates routes based on the file/diretory names which can also have regex",
+    "one large pizza is larger than 2 medium pizzas",
+    "the core of the earth ages more slowly compared to the rest of the plane due to time dilation",
+    '"Eleven plus two" is an anagram of "Twelve plus one"',
+    "ketchup used to be sold in the United States as medicine in the 1800s",
+  ];
+
+  return facts[Math.floor(Math.random() * facts.length)];
+}
+
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
@@ -26,12 +45,10 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingM}>
+        <p>Welcome to my mind dump! Did you know {randomFact()}?</p>
         <p>
-          Hi, I'm Saakshaat. You may or may not know me but have a cookie,
-          nonetheless.
-        </p>
-        <p>
-          Anyhow, so this is a <a href="https://nextjs.org/">Next.js</a> app
+          Anyhow, the purpose of this app was to get familiar with{" "}
+          <a href="https://nextjs.org/">Next.js</a> app
         </p>
       </section>
 
